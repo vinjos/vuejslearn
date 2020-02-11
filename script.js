1ddf3da352c64456f41d51472f9fd8446463693a
Vincze József <vinczejozsef3@gmail.com>
2020-02-11 15:15:08 +0100

    add filter & change style

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function Todo(name) {
 var app = new Vue({
     el: '#todoApp',
     data: {
+        filter: 'all',
         newTodoName: '',
-        message: 'Hello Vue!',
+        editingTodoName: null,
         todoCollection: [
           new Todo('todo1'),
           new Todo('todo2'),
@@ -19,10 +20,20 @@ var app = new Vue({
     },
     methods: {
         OnDblClickTodoName: function(todo) {
+            if(todo.completed) {
+                return;
+            }
             todo.editing = true;
+            this.editingTodoName = todo.name;
         },
         OnEnterTodoName: function(todo) {
             todo.editing = false;
+            todo.name = this.editingTodoName;
+            this.editingTodoName = null;
+        },
+        OnCancelTodo: function(todo) {
+            todo.editing = false;
+            this.editingTodoName = null;
         },
         OnEnterAddTodo: function() {
             if(!this.newTodoName) {
@@ -35,11 +46,38 @@ var app = new Vue({
             this.todoCollection.filter(function(todo) {
                 todo.completed = true;
             });
+        },
+        SetFilter: function(filter) {
+            this.filter = filter;
+        },
+        GetFilterButtonClass: function(filter) {
+            return {
+                'btn-outline-secondary': filter !== this.filter,
+                'btn-secondary': filter === this.filter
+            };
+        },
+        OnClickRemoveCompleted: function() {
+            this.todoCollection = this.todoCollection.filter(function(todo) {
+                return !todo.completed;
+            })
         }
     },
     computed: {
         counter: function() {
             return this.todoCollection.length;
+        },
+        filteredTodoCollection: function() {
+            var vm = this;
+            return vm.todoCollection.filter(function(todo){
+                switch(vm.filter) {
+                    case 'all':
+                        return true;
+                    case 'active':
+                        return !todo.completed;
+                    case 'completed':
+                        return todo.completed;
+                }
+            });
         }
     }
   });
